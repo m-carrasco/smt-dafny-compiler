d9bc062a923df3e4275dc7ac07fd21a2e1207cbf
Manuel Carrasco <m.carrasco@imperial.ac.uk>
2024-09-02 07:51:18 +0100

    Include SLOT binary in the Docker image.

--- ci/build-images.sh
+++ ci/build-images.sh
@@ -5,5 +5,10 @@ CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 
 pushd $CURRENT_DIR/..
 git submodule update --init --recursive
+
+pushd external/SLOT
+docker build -t smt-dafny-compiler-slot:latest .
+popd
+
 docker build --file $CURRENT_DIR/Dockerfile --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t smt-dafny-compiler:latest .
 popd
