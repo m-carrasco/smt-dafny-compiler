62d10fe6e718234caf5042ab6486fa02d8d6611b
Manuel Carrasco <m.carrasco@imperial.ac.uk>
2024-09-29 16:12:01 +0000

    Implement sgt.

--- src/Compiler/Z3ExprConverter.cs
+++ src/Compiler/Z3ExprConverter.cs
@@ -153,6 +153,13 @@ public class Z3ExprConverter
                             dafnyExpr = new UnaryExpression(Slt(((BitVecExpr)z3Expr.Args[0]).SortSize, a1, a0), Operator.BooleanNegation);
                             break;
                         }
+                    case Z3_decl_kind.Z3_OP_SGT:
+                        {
+                            var a0 = _childConverter(z3Expr.Args[0]);
+                            var a1 = _childConverter(z3Expr.Args[1]);
+                            dafnyExpr = Slt(((BitVecExpr)z3Expr.Args[0]).SortSize, a1, a0);
+                            break;
+                        }
                     case Z3_decl_kind.Z3_OP_UNINTERPRETED:
                         {
                             if (z3Expr.IsConst && z3Expr.NumArgs == 0)
